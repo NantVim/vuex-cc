182c9a023ea2541165afeb10f8ad30bb6ff0beae
nantvim <nantvim@gmail.com>
2019-10-07 20:49:00 +0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="post" v-for="post in allPosts" :key="post.id">
+      <h2>{{post.title}}</h2>
+      <p>{{post.body}}</p>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import {mapGetters} from 'vuex'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  // computed: {
+  //   allPosts() {
+  //     return this.$store.getters.allPosts;
+  //   }
+  // },
+  computed: mapGetters(['allPosts']),
+  async mounted() {
+    this.$store.dispatch('fetchPosts')
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: rgb(11, 20, 99);
+  margin: 60px auto;
+  width: 400px;
+}
+
+.post {
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  margin-bottom: 1rem;
 }
 </style>
